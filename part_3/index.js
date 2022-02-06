894fa54f2e5cec511d815521ab15091d0173a1e0
Bach Phung <quangbach19112000@gmail.com>
2022-02-06 04:07:07 +0200

    Add file setup heroku

--- part_3/index.js
+++ part_3/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const APIroute = require('./routes/routes')
 const app = express()
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors  = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 app.use(express.json())
-
+app.use(cors())
 morgan.token('body',(req)=>{
     if(req.method ==='POST')
         return JSON.stringify(req.body)
     else
         return null
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 app.use('/',APIroute)
 app.listen(PORT,()=>{
