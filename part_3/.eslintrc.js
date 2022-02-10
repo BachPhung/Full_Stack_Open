a8f478fe8d4973b080c08d18464374b8ce1e27c8
Bach Phung <quangbach19112000@gmail.com>
2022-02-10 23:07:12 +0200

    Fix redundant things

--- part_3/.eslintrc.js
+++ part_3/.eslintrc.js
@@ -1,21 +1,29 @@
 module.exports = {
-	env: {
-	  browser: true,
-	  commonjs: true,
-	  es2021: true,
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
 	},
-	extends: "plugin:react/recommended",
-	parserOptions: {
-	  ecmaFeatures: {
-		jsx: true,
-	  },
-	  ecmaVersion: 12,
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
 	},
-	plugins: ["react"],
-	rules: {
-	  "no-trailing-spaces": "error",
-	  "object-curly-spacing": ["error", "always"],
-	  "arrow-spacing": ["error", { before: true, after: true }],
-	  "no-console": 0,
-	},
-  };
+	'rules': {
+		'indent': [
+            'error',
+            4
+        ],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
