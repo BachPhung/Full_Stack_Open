e52f98855772721cc1424482662142bb5e29fcee
Bach Phung <quangbach19112000@gmail.com>
2022-03-13 16:12:49 +0200

    9.8 - 9.9

---
+++ part_9/Patientor-api/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express()
+
+const PORT = 3001;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
