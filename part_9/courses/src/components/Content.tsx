d12018c44b259781df7c6982ecab06c3fca8a0ab
Bach Phung <quangbach19112000@gmail.com>
2022-03-14 07:59:06 +0200

    9.15

--- part_9/courses/src/components/Content.tsx
+++ part_9/courses/src/components/Content.tsx
@@ -4,7 +4,34 @@ import { Courses } from '../types'
 const Content = ({ infos }: Courses) => (
     <>
         {infos.map(course => {
-            return <div key={course.name}>{course.name} {course.exerciseCount}</div>
+            switch (course.type) {
+                case 'normal': {
+                    return <div key={course.name}>
+                        <h4>{course.name + " " + course.exerciseCount}</h4>
+                        <div>{course.description}</div>
+                    </div>
+                }
+                case 'groupProject': {
+                    return <div key={course.exerciseCount}>
+                        <h4>{course.name + " " + course.exerciseCount}</h4>
+                        <div>group exercises ${course.groupProjectCount}</div>
+                    </div>
+                }
+                case 'submission': {
+                    return <div key={course.name}>
+                        <h4>{course.name + " " + course.exerciseCount}</h4>
+                        <div>{course.description}</div>
+                        <div>submit to {course.exerciseSubmissionLink}</div>
+                    </div>
+                }
+                case 'special': {
+                    return <div key={course.name}>
+                        <h4>{course.name + " " + course.exerciseCount}</h4>
+                        <div>{course.description}</div>
+                        <div>required skills: {course.requirements.map(skill=><span key={skill}> {skill}</span>)}</div>
+                    </div>
+                }
+            }
         })}
     </>
 )
--- part_9/courses/src/types.ts
+++ part_9/courses/src/types.ts
@@ -1,7 +1,32 @@
-export type CoursePart = {
-    name: string,
-    exerciseCount: number
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
 }
+
+interface NewCoursePartBase extends CoursePartBase {
+    description: string
+}
+
+interface CourseNormalPart extends NewCoursePartBase {
+    type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends NewCoursePartBase {
+    type: "submission";
+    exerciseSubmissionLink: string;
+}
+interface CourseSpecialPart extends NewCoursePartBase {
+    type: "special";
+    requirements: string[];
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
 export type Courses = {
     infos: Array<CoursePart>
 }
